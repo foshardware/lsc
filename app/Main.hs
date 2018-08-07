24221cb4b82ab16822eb7b10e4438b9f944f5f02
Andreas Westerwick <westerwick@pconas.de>
2018-08-07 22:07:25 +0200

    cli params

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,14 @@ import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
 
-import Data.Text.IO as Text
+import Data.Foldable
+
+import qualified Data.Text.IO as Text
+import qualified Data.Text.Lazy.IO as Lazy
 
 import System.Console.GetOpt
 import System.Environment
+import System.IO
 
 import BLIF.Parser
 import LEF.Parser
@@ -20,6 +24,9 @@ import LSC.LEF
 import LSC.SVG
 import LSC.Types
 
+versionString :: String
+versionString = "lsc 0.1.0.0"
+
 type App = MaybeT IO
 main = runMaybeT program
 
@@ -28,16 +35,31 @@ program = do
 
     (opts, _) <- liftIO $ compilerOpts =<< getArgs
 
-    let lefFile   = head [v | (k, v) <- opts, k == Lef]
-        blifFile  = head [v | (k, v) <- opts, k == Blif]
+    -- print version string
+    when (any ( \ (k, _) -> k == Version) opts)
+      $ liftIO $ hPutStrLn stderr $ versionString
+
+    -- escape hatches
+    guard $ any ( \ (k, _) -> k == Compile) opts
+    guard $ not $ any ( \ (k, _) -> k == Version) opts
+
+    let lefFiles   = [v | (k, v) <- opts, k == Lef]
+        blifFiles  = [v | (k, v) <- opts, k == Blif]
 
     result <- lift $ do
-        bootstrap <- either (error . show) fromLEF . parseLEF <$> Text.readFile lefFile
-        netlist   <- either (error . show) (gnostic bootstrap . fromBLIF) . parseBLIF <$> Text.readFile blifFile
+        bootstrap <- either (error . show) fromLEF . parseLEF <$> Text.readFile (head lefFiles)
+        netlist   <- either (error . show) (gnostic bootstrap . fromBLIF) . parseBLIF <$> Text.readFile (head blifFiles)
 
         bootstrap `runLSC` stage1 netlist
 
-    liftIO $ plotStdout $ lexNodes result
+    -- svg output
+    when (any ( \ (k, v) -> k == Compile && v == "svg") opts)
+      $ liftIO $ plotStdout $ lexNodes result
+
+    -- sat output
+    when (any ( \ (k, _) -> k == Verbose) opts)
+      $ liftIO $ hPutStrLn stderr $ show result
+    
 
 type Flag = (FlagKey, FlagValue)
 
@@ -46,6 +68,7 @@ data FlagKey
   | Version
   | Blif
   | Lef
+  | Compile
   deriving (Eq, Show)
 
 type FlagValue = String
@@ -56,6 +79,7 @@ options =
     , Option ['V', '?'] ["version"] (NoArg  (Version, []))  "show version number"
     , Option ['b']      ["blif"]    (ReqArg (Blif, ) "FILE") "BLIF file"
     , Option ['l']      ["lef"]     (ReqArg (Lef,  ) "FILE") "LEF file"
+    , Option ['c']      ["compile"]  (OptArg ((Compile,  ) . maybe "svg" id) "svg, magic") "output format"
     ]
 
 compilerOpts :: [String] -> IO ([Flag], [String])
@@ -64,3 +88,4 @@ compilerOpts argv =
         (o, n, []  ) -> pure (o, n)
         (_, _, errs) -> ioError $ userError $ concat errs ++ usageInfo header options
      where header = "Usage: lsc [OPTION...] files..."
+
