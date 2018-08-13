592d184901903d152cb60dbac36bb8260d5f94b3
Andreas Westerwick <westerwick@pconas.de>
2018-08-13 19:30:26 +0000

    staging changes

--- src/LSC/Exlining.hs
+++ src/LSC/Exlining.hs
@@ -1,8 +1,14 @@
 
 module LSC.Exlining where
 
+import Data.Text (unpack)
+import Data.Hashable
+
 import LSC.SuffixTree
 import LSC.Types
 
 
-hierarchical = id
+hierarchical :: Netlist -> SuffixTree Gate
+hierarchical (Netlist nodes _) = constructSuffixTree goedel nodes
+  where goedel gate = hash $ gateIdent gate
+
--- src/LSC/SuffixTree.hs
+++ src/LSC/SuffixTree.hs
@@ -1,33 +1,47 @@
 
 module LSC.SuffixTree where
 
+import Data.Foldable as Fold
+import Control.Monad.ST
 import Data.Ord
-import Data.Functor.Identity
-import Data.Vector hiding (zip)
+import Data.List (tails)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as Map
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+import qualified Data.Vector.Algorithms.Intro as Intro
+import qualified Data.Vector.Algorithms.Radix as Radix
 
 
-newtype SuffixTreeT m a = SuffixTreeT (PredicateT m a, SuffixArray a, LCP a)
+newtype SuffixTree a = SuffixTree (IntMap a, SuffixArray, LCP)
+  deriving Show
 
-newtype SuffixArray a = SuffixArray (Vector (Position, Suffix a))
+newtype SuffixArray = SuffixArray (Vector Suffix)
+  deriving Show
 
-newtype LCP a = LCP (Vector Length)
-
-type PredicateT m a = a -> a -> m Ordering
-type Predicate = PredicateT Identity
+newtype LCP = LCP (Vector Length)
+  deriving Show
 
 type Position = Int
-type Suffix a = [a]
+type Suffix = (Position, Vector Int)
 
 type Length = Int
 
 
-type SuffixTree a = SuffixTreeT Identity a
-
-constructSuffixTree :: Predicate a -> [a] -> SuffixTree a
-constructSuffixTree predicate xs = SuffixTreeT (proposition, suffixArray, lcp)
+constructSuffixTree :: Foldable f => (a -> Int) -> f a -> SuffixTree a
+constructSuffixTree goedel xs = SuffixTree (dictionary, suffixArray, lcp)
   where
     lcp = undefined
-    suffixArray = SuffixArray $ fromList [(i, ys) | (i, ys) <- zip [0..] $ tails xs]
-    proposition a b = pure $ predicate a b
+    dictionary = Map.fromList [(goedel x, x) | x <- Fold.toList xs]
+    suffixArray = SuffixArray $ sort $ Vector.fromList
+      [ (i, Vector.fromList ys)
+      | (i, ys) <- zip [0..] $ tails $ fmap goedel $ Fold.toList xs
+      ]
 
+
+sort :: Vector Suffix -> Vector Suffix
+sort v = runST $ do
+  m <- Vector.thaw v
+  Intro.sort m
+  Vector.freeze m
 
