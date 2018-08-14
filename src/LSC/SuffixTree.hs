19806d5440549ea46ca2d8149afaa837f6006576
Andreas Westerwick <westerwick@pconas.de>
2018-08-14 18:28:27 +0000

    staging changes

--- src/LSC/SuffixTree.hs
+++ src/LSC/SuffixTree.hs
@@ -37,27 +37,22 @@ constructSuffixTree goedel xs = SuffixTree string suffixArray lcp
       ]
 
     lcp = Vector.fromList
-      [ (i, commonPrefix x y)
-      | k <- [0 .. Vector.length suffixArray - 1]
+      [ (i, Unboxed.foldr commonPrefix 0 $ Unboxed.zip x y)
+      | k <- [1 .. Vector.length suffixArray - 1]
       , let (i, x) = suffixArray ! k
-      , let (_, y) = suffixArray ! (k-1) -- undefined for k=0: this is the sentinel string
+      , let (_, y) = suffixArray ! (k-1)
       ]
+
+
+commonPrefix :: (Int, Int) -> Int -> Int
+commonPrefix (a, b) s | a == b = 1 + s
+commonPrefix _ _ = 0
 
 
 longestSubstring :: SuffixTree a -> (Int, Int, Vector a)
 longestSubstring (SuffixTree string _ lcp)
   = (ix, len, Vector.take len $ Vector.drop ix $ string)
   where (ix, len) = maximumBy ( \ a b -> snd a `compare` snd b ) lcp
-
-
-commonPrefix :: Suffix -> Suffix -> Int
-commonPrefix xs_ ys_
-  | (x, xs) <- Unboxed.splitAt 1 xs_
-  , (y, ys) <- Unboxed.splitAt 1 ys_
-  , Unboxed.length x > 0
-  , x == y
-  = 1 + commonPrefix xs ys
-commonPrefix _ _ = 0
 
 
 sortBy :: (a -> Suffix) -> Vector a -> Vector a
