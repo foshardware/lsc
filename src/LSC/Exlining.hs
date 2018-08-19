03920322824002250925222edb6340dc3b2b1892
Andreas Westerwick <westerwick@pconas.de>
2018-08-19 21:18:47 +0000

    staging changes

--- src/LSC/Exlining.hs
+++ src/LSC/Exlining.hs
@@ -3,23 +3,28 @@
 module LSC.Exlining where
 
 import Data.Function
+import qualified Data.Set as Set
 import Data.Vector hiding (replicate)
-import Prelude hiding ((++), splitAt, length, concat, drop, take)
+import Prelude hiding ((++), splitAt, length, concat, drop, take, null)
 
 import LSC.Types
 import LSC.Duval
 
 
 exline (Netlist _ _ _ nodes _)
-  = length <$> lyn
-  where lyn = recurseLyndon 8 (GateChar <$> nodes)
+  = (length $ unique, length $ lyn, length <$> lyn)
+  where
+    lyn = recurseLyndon 16 (GateChar <$> nodes)
+    unique = fromList $ Set.toList $ Set.fromList $ toList lyn
+
 
 
 recurseLyndon :: Ord a => Int -> Vector a -> Vector (Vector a)
 recurseLyndon k nodes
   | i <- maxLyndon nodes
   , (xs, ys) <- splitAt i nodes
-  , i > k
+  , i > 0
+  , length nodes > k
   = recurseLyndon k xs ++ recurseLyndon k ys
 recurseLyndon _ nodes = singleton nodes
 
@@ -28,7 +33,7 @@ maxLyndon nodes
   = fst
   $ maximumBy (compare `on` weight)
   $ fromList
-  [ (i, duval xs ++ duval ys)
+  [ (i, duval ys ++ duval xs)
   | i <- [ 0 .. length nodes - 1 ]
   , let (xs, ys) = splitAt i nodes
   ] where weight (_, xs) = length xs
