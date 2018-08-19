382705189ad44440ca4a799ad01f897f4d52352c
Andreas Westerwick <westerwick@pconas.de>
2018-08-19 19:55:37 +0000

    refactor duval code

--- src/LSC/Duval.hs
+++ src/LSC/Duval.hs
@@ -5,6 +5,7 @@ import Control.Monad.Loops
 import Control.Monad.ST
 import Data.STRef
 import Data.Vector
+import Data.Vector.Mutable (new, write)
 import Prelude hiding (length)
 
 type String' a = Vector a
@@ -13,42 +14,39 @@ duval :: Ord a => String' a -> Vector (String' a)
 duval s = runST $ do
 
   let n = length s
-  i_ <- newSTRef 0
-  factorization <- newSTRef mempty
 
-  whileM_
-      ( do
-          i <- readSTRef i_
-          pure $ i < n ) $ do
+  i_ <- newSTRef 0
+
+  factorization <- new n
+  c_ <- newSTRef 0
+
+  whileM_ ((n >) <$> readSTRef i_) $ do
 
       j_ <- newSTRef . (+ 1) =<< readSTRef i_
       k_ <- newSTRef =<< readSTRef i_
 
-      whileM_
-          ( do
-              j <- readSTRef j_
-              k <- readSTRef k_
-              pure $ j < n && s!k <= s!j ) $ do
+      whileM_ (( \ j k -> j < n && s!k <= s!j ) <$> readSTRef j_ <*> readSTRef k_) $ do
 
           j <- readSTRef j_
           k <- readSTRef k_
-          if s!k < s!j then
-              writeSTRef k_ =<< readSTRef i_
-          else
-              modifySTRef k_ (+ 1)
-      
+
+          if s!k < s!j
+          then writeSTRef k_ =<< readSTRef i_
+          else modifySTRef k_ (+ 1)
+
           modifySTRef j_ (+ 1)
 
-      whileM_
-          ( do
-              i <- readSTRef i_
-              k <- readSTRef k_
-              pure $ i <= k ) $ do
+      whileM_ ((<=) <$> readSTRef i_ <*> readSTRef k_) $ do
+
           i <- readSTRef i_
           j <- readSTRef j_
           k <- readSTRef k_
-          modifySTRef factorization (`snoc` slice i (j - k) s)
+          c <- readSTRef c_
+
+          write factorization c (slice i (j - k) s)
+          modifySTRef c_ (+ 1)
+
           modifySTRef i_ (+ (j - k))
 
-  readSTRef factorization
+  slice 0 <$> readSTRef c_ <*> freeze factorization
 
