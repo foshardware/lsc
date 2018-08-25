2926fa11606a91b8cefc808b000e8dc067443c6c
Andreas Westerwick <westerwick@pconas.de>
2018-08-25 19:51:25 +0000

    embed simple test

--- app/Test.hs
+++ app/Test.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
 
 module Test where
 
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
+
+import Data.FileEmbed
+import Data.Text (Text)
+import Data.Text.Encoding
+import qualified Data.Text.IO as Text
 
 import LSC.Exlining
 
@@ -14,5 +20,11 @@ runTests :: IO ()
 runTests = void $ runMaybeT tests
 
 tests :: Test ()
-tests = pure ()
+tests = do
+
+  liftIO $ Text.putStrLn rot
+
+
+rot :: Text
+rot = decodeUtf8 $(embedFile "tests/rot.blif.test")
 
