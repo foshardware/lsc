86fc1c6c216e0d0addf247d6b3bb80a4cb19f18a
Andreas Westerwick <westerwick@pconas.de>
2018-08-25 19:58:42 +0000

    include osu035 in test

--- app/Test.hs
+++ app/Test.hs
@@ -9,9 +9,16 @@ import Control.Monad.Trans.Maybe
 import Data.FileEmbed
 import Data.Text (Text)
 import Data.Text.Encoding
-import qualified Data.Text.IO as Text
 
+import BLIF.Builder
+import BLIF.Parser
+import LEF.Parser
+
+import LSC
+import LSC.BLIF
+import LSC.LEF
 import LSC.Exlining
+import LSC.Types
 
 
 type Test = MaybeT IO
@@ -22,9 +29,22 @@ runTests = void $ runMaybeT tests
 tests :: Test ()
 tests = do
 
-  liftIO $ Text.putStrLn rot
+  lefOsu035 <- lift $ either
+    (ioError . userError . show)
+    (pure . fromLEF)
+    (parseLEF osu035)
+
+  blifRot <- lift $ either
+    (ioError . userError . show)
+    (pure . gnostic lefOsu035 . fromBLIF)
+    (parseBLIF rot)
+
+  liftIO $ printBLIF $ toBLIF $ exlineRounds (replicate 3 4) blifRot
 
 
 rot :: Text
 rot = decodeUtf8 $(embedFile "tests/rot.blif.test")
+
+osu035 :: Text
+osu035 = decodeUtf8 $(embedFile "tests/osu035.lef.test")
 
