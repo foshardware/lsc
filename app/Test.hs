7ef5ac4695893f064e9083f7b5f40cd9e9fcdb2e
Andreas Westerwick <westerwick@pconas.de>
2018-11-03 19:00:47 +0100

    64

--- app/Test.hs
+++ app/Test.hs
@@ -44,7 +44,7 @@ tests = do
     (pure . gnostic lefOsu035 . fromBLIF)
     (parseBLIF picorv32File)
 
-  let exlined = exline_ (replicate 8 8) blifPicorv32
+  let exlined = exline_ (replicate 64 8) blifPicorv32
   let inlined = inlineAll exlined
   -- liftIO $ printBLIF $ toBLIF $ exlined
   liftIO $ hPutStrLn stderr $ showNetHierarchy $ exlined
