afbb3f05b8fe296157b3c8d47dc146fb497b507d
Andreas Westerwick <westerwick@pconas.de>
2018-11-04 14:07:23 +0100

    profiling

--- stack-profile.sh
+++ stack-profile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# stack build --executable-profiling --library-profiling --ghc-options="-fprof-auto -rtsopts"
-# stack exec -- lsc +RTS -p
+# stack build --executable-profiling --library-profiling --ghc-options="-fprof-auto -rtsopts" && stack exec -- lsc +RTS -p
 
-stack build --profile --ghc-options=-fprof-auto-top && stack exec -- lsc-test +RTS -p
+stack build --profile --ghc-options=-fprof-auto && stack exec -- lsc-test +RTS -p
+
+# stack build --profile --ghc-options=-fprof-auto-top && stack exec -- lsc-test +RTS -p
 
