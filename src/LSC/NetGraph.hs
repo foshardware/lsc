65ea5acac75bcaefb24a3ef3b24d23524157acd1
Andreas Westerwick <westerwick@pconas.de>
2018-12-25 06:34:43 +0000

    utility on net graphs

--- src/LSC/NetGraph.hs
+++ src/LSC/NetGraph.hs
@@ -1,7 +1,9 @@
 
 module LSC.NetGraph where
 
+import Control.Monad
 import Data.Foldable
+import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Map.Internal.Debug
 import Data.Text (unpack)
@@ -10,17 +12,27 @@ import qualified Data.Vector as Vector
 import LSC.Types
 
 
+type Leaves = Map String Integer
+
+
 getLeaves :: NetGraph -> [NetGraph]
 getLeaves netlist | null $ subModels netlist = [netlist]
 getLeaves netlist = getLeaves =<< toList (subModels netlist)
 
 
+flattenHierarchy :: NetGraph -> [NetGraph]
+flattenHierarchy netlist
+  = netlist
+  : join [ flattenHierarchy model | model <- toList $ subModels netlist ]
+
+
 showGraph :: NetGraph -> String
 showGraph netlist = showTreeWith
-  ( \ k x -> unwords [unpack k, showGraph x])
+  ( \ k x -> unwords [unpack k, showGraph x] )
   True
   True
   (subModels netlist)
+
 
 showNetHierarchy :: NetGraph -> String
 showNetHierarchy netlist = unlines [ showNetHierarchy m | m <- toList $ subModels netlist ]
@@ -33,3 +45,4 @@ showNetHierarchy netlist = unlines [ showNetHierarchy m | m <- toList $ subModel
   ++ unlines [ unpack g ++ ": " ++ show c | (g, c) <- Map.assocs gates ]
   where
     gates = Map.fromListWith (+) [ (gateIdent g, 1 :: Int) | g <- toList $ gateVector netlist ]
+
