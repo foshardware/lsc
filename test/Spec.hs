b841a95deffe536179592a71fdc3691f22b6e634
Andreas Westerwick <westerwick@pconas.de>
2019-01-25 23:33:05 +0100

    timing

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,7 @@ import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Lens
 import Control.Monad.IO.Class
+import Data.Bits
 import Data.Default
 import Prelude hiding (id, (.))
 
@@ -22,44 +23,43 @@ main = defaultMain $ testGroup "LSC"
 
 
 concurrency :: TestTree
-concurrency = testGroup "Concurrency"
-  [ testCase "dual core" dualCore
-  , testCase "quad core" quadCore
+concurrency = testGroup "Concurrency" $
+  [ testCase "Dual core" $ dualCore n
+  | n <- take 4 $ drop 2 $ iterate (`shiftR` 1) (shiftL 1 20)
+  ] ++
+  [ testCase "Quad core" $ quadCore n
+  | n <- take 4 $ drop 2 $ iterate (`shiftR` 1) (shiftL 1 20)
   ]
 
 
-time :: Int -> Int
-time n = n * 100000
-
-
-dualCore :: IO ()
-dualCore = do
+dualCore :: Int -> IO ()
+dualCore n = do
   counter <- newMVar 0
   ws <- createWorkers 2
-  let inc = incrementWithDelay 2 counter
+  let inc = incrementWithDelay (2*n) counter
       act = fst ^<< inc &&& inc &&& inc &&& inc
       tech = thaw def
       opts = def & workers .~ ws
   forkIO $ runLSC opts tech $ compiler act mempty
-  threadDelay $ time 1
+  threadDelay n
   result1 <- readMVar counter
-  threadDelay $ time 2
+  threadDelay (2*n)
   result2 <- readMVar counter
   assertBool "two at a time" $ result1 == 2 && result2 == 4
 
 
-quadCore :: IO ()
-quadCore = do
+quadCore :: Int -> IO ()
+quadCore n = do
   counter <- newMVar 0
   ws <- createWorkers 4
-  let inc = incrementWithDelay 2 counter
+  let inc = incrementWithDelay (2*n) counter
       act = fst . snd ^<< (inc &&& inc) &&& inc &&& (inc &&& inc &&& inc)
       tech = thaw def
       opts = def & workers .~ ws
   forkIO $ runLSC opts tech $ compiler act mempty
-  threadDelay $ time 1
+  threadDelay n
   result1 <- readMVar counter
-  threadDelay $ time 2
+  threadDelay (2*n)
   result2 <- readMVar counter
   assertBool "four at a time" $ result1 == 4 && result2 == 6
 
@@ -67,6 +67,6 @@ quadCore = do
 incrementWithDelay :: Int -> MVar Int -> Compiler ()
 incrementWithDelay n counter = ls_ . liftIO $ do
   modifyMVar_ counter $ pure . succ
-  threadDelay $ time n
+  threadDelay n
 
 
